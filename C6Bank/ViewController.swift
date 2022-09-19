b7a8ec2fd241f466794dcc4181f6a9476c1105e8
Gabriel Castro <gabrieldcc@gmail.com>
2022-09-19 10:21:36 -0300

    Initial Commit

---
+++ C6Bank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  C6Bank
+//
+//  Created by Gabriel de Castro Chaves on 19/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ C6BankUITests/C6BankUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  C6BankUITestsLaunchTests.swift
+//  C6BankUITests
+//
+//  Created by Gabriel de Castro Chaves on 19/09/22.
+//
+
+import XCTest
+
+class C6BankUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
