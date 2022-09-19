e7ad5c1dd6b59b6f225e32c1f92b1275e7b1f199
Gabriel Castro <gabrieldcc@gmail.com>
2022-09-19 19:38:10 -0300

    ïmplementado feature de esconder e mostrar saldo, alterando a imagem do button

--- C6Bank/ViewController.swift
+++ C6Bank/ViewController.swift
@@ -8,26 +8,57 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    
+    //MARK: - Var
+    private var showBalance = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         roundedUpViews()
         turnViewIntoCircle()
+        saldoTextField.isUserInteractionEnabled = false
+        
+        if(showBalance == true) {
+            saldoTextField.isSecureTextEntry = true
+            setupEyeShowDataButtonImage()
+            showBalance.toggle()
+        }
     }
-
+    
     //MARK: - IBOutlets
+    @IBOutlet weak var saldoButton: UIButton!
+    @IBOutlet weak var saldoTextField: UITextField!
     @IBOutlet weak var saldoContentView: UIView!
     @IBOutlet weak var verExtratoImage: UIImageView!
     @IBOutlet weak var pagarImage: UIImageView!
     @IBOutlet weak var depositarImage: UIImageView!
     @IBOutlet weak var transferirImage: UIImageView!
     @IBOutlet weak var brazilImageContentView: UIView!
-    
     @IBOutlet weak var userButton: UIButton!
     @IBOutlet weak var todosContentView: UIView!
+    
     //MARK: - IBActions
     @IBAction func saldoButton(_ sender: Any) {
-        print("its working")
+        
+    }
+    
+    @IBAction func eyeShowData(_ sender: Any) {
+        if(showBalance == true) {
+            saldoTextField.isSecureTextEntry = true
+            setupEyeShowDataButtonImage()
+        } else {
+            saldoTextField.isSecureTextEntry = false
+            setupEyeShowDataButtonImage()
+        }
+        showBalance = !showBalance
+    }
+    
+    
+    //MARK: - Functions
+    private func setupEyeShowDataButtonImage() {
+        let imageName = showBalance ? "eye.slash.fill" : "eye.fill"
+        let image = UIImage.init(systemName: imageName)?.withRenderingMode(.alwaysTemplate)
+        saldoButton.setImage(image, for: .normal)
     }
     
     private func roundedUpViews() {
@@ -39,13 +70,9 @@ final class ViewController: UIViewController {
         roundUpView(view: todosContentView)
     }
     
-    
     private func roundUpView(view: UIView) {
         view.layer.cornerRadius = 10
         view.layer.masksToBounds = true
-        
-        
-        
     }
     
     private func turnViewIntoCircle() {
@@ -55,6 +82,6 @@ final class ViewController: UIViewController {
         userButton.layer.cornerRadius = userButton.frame.height / 2
         userButton.layer.masksToBounds = true
     }
-    
+
 }
 
