41a22df9ead4d0dfe63d42f0d170191d5a0d3d15
Gabriel Castro <gabrieldcc@gmail.com>
2022-09-19 18:23:32 -0300

    subindo projeto para o Github

--- C6Bank/ViewController.swift
+++ C6Bank/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        roundedUpViews()
+        turnViewIntoCircle()
     }
 
-
+    //MARK: - IBOutlets
+    @IBOutlet weak var saldoContentView: UIView!
+    @IBOutlet weak var verExtratoImage: UIImageView!
+    @IBOutlet weak var pagarImage: UIImageView!
+    @IBOutlet weak var depositarImage: UIImageView!
+    @IBOutlet weak var transferirImage: UIImageView!
+    @IBOutlet weak var brazilImageContentView: UIView!
+    
+    @IBOutlet weak var userButton: UIButton!
+    @IBOutlet weak var todosContentView: UIView!
+    //MARK: - IBActions
+    @IBAction func saldoButton(_ sender: Any) {
+        print("its working")
+    }
+    
+    private func roundedUpViews() {
+        roundUpView(view: saldoContentView)
+        roundUpView(view: verExtratoImage)
+        roundUpView(view: pagarImage)
+        roundUpView(view: depositarImage)
+        roundUpView(view: transferirImage)
+        roundUpView(view: todosContentView)
+    }
+    
+    
+    private func roundUpView(view: UIView) {
+        view.layer.cornerRadius = 10
+        view.layer.masksToBounds = true
+        
+        
+        
+    }
+    
+    private func turnViewIntoCircle() {
+        brazilImageContentView.layer.cornerRadius = brazilImageContentView.frame.height / 2
+        brazilImageContentView.layer.masksToBounds = true
+        
+        userButton.layer.cornerRadius = userButton.frame.height / 2
+        userButton.layer.masksToBounds = true
+    }
+    
 }
 
